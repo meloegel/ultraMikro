3aa874d93ad3d74a236f24d0fb9db96764e3cc48
meloegel <meloegel@gmail.com>
2020-10-04 16:55:04 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import './App.css';
+
+import Home from './components/Home'
+import Publications from './components/Publications'
+import Gallery from './components/Gallery'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/publications" >Publications</NavLink>
+          <NavLink to='/gallery'>Gallery</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+        </nav>
+        <div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/publications' component={Publications} />
+            <Route path='/gallery' component={Gallery} />
+            <Route path='/contact' component={Contact} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Publications.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+
+const Publications = () => {
+
+    return (
+        <div>
+            <h1 id='pubTitle'>Publications</h1>
+            <ol>
+                <li>Aldrich, D.S., "Particulate Formation as the Result of Packaging
+                Interactions", PROCEEDINGS of the European Organization for Quality Control Conference on Visible and Subvisible
+                Particles in Parenteral Products, Brussels, Belgium, 261 (1985).</li>
+                <li>Borchert, S.J., Abe, A., Aldrich, D.S., Fox, L.E., Freeman, J.E. and
+                White, R.D., "Particulate Matter in Parenteral Products: A Review,"
+                Journal of Parenteral Science and Technology, Sept/Oct 212 (1986).</li>
+                <li>Borchert, S.J., Maxwell, R.J., Davison, R.L. and Aldrich, D.S., "Standard Particulate Sets for Visual Inspection Systems: Their Preparation, Evaluation and Applications", Journal of Parenteral Science and Technology, Nov/Dec 265 (1986). 1987 PDA JOURNAL AWARD RECIPIENT</li>
+                <li>Aldrich, D.S., Borchert, S.J., Abe, A. and Freeman, J.E., "Pharmaceutical Trace Analysis," Journal of Research - National Bureau of Standards, May-June 93, No. 3 (1987).</li>
+                <li>Borchert, S.J., Aldrich, D.S., Fox, L.E., Freeman, J.E. and White, R.D., "Mechanisms of Particulate Formation and Particulate Reduction Steps", PROCEEDINGS of the 9th International Symposium on Contamination Control, Institute of Environmental Sciences, Los Angeles, CA, September 26-28, 1988</li>
+                <li>Aldrich, D.S. and Freeman, J.E., USP XXII General Chapter, Scanning Electron Microscopy &lt;1181&gt;, pg. 1700, United States Pharmacopeial Convention, Inc., Rockville, Md., 1989.</li>
+                <li>Aldrich, D.S., "Identification of Parenteral Particulates," PROCEEDINGS of the 2nd International Conference on Particle Detection, Metrology and Control, Parenteral Drug Association/Institute of Environmental Sciences, Arlington, VA, February 5-7, 1990.</li>
+                <li>Borchert, S.J. and Aldrich, D.S., "Glass Container Contributions to Parenteral Particulates," PROCEEDINGS of the 2nd International Conference on Particle Detection, Metrology and Control, Parenteral Drug Association/Institute of Environmental Sciences, Arlington, VA, February 5-7, 1990.</li>
+                <li>Aldrich, D.S. and Smith, M.A. (1995) Chapter 9 - Pharmaceutical Applications of Infrared Microspectroscopy, in Humecki, H., Editor, "Practical Guide to Infrared Microspectroscopy", 2nd Edition, Marcel Dekker, New York, NY, 323-375.[reprinted 1999 in Applied Spectroscopy Reviews 34(4)275-327.]</li>
+                <li>Maxwell, R.J. and Aldrich, D.S., "The Chemistry of Glass in Pharmaceutical Packaging", SCANNING '95 Proceedings, April 1995.</li>
+                <li>Schinzer, W.C., Bergren, M.S., Aldrich, D.S., Chao, R.S., Dunn, M.J., Jeganathan, J., and Madden, L.M., "The Characterization and Interconversion of Polymorphs of Premafloxacin, A New Quinolone Antibiotic", J Pharm Sci: 86(12); Dec 97: 1426-31. </li>
+                <li>Aldrich, D.S., "Membrane-Based Counting of the Particulate Matter Load in Parenteral Products", The Microscope, Vol 45(3):73-83 (1997).</li>
+                <li>Lua, Y.Y, Cao, X., Rohrs, B.R. and Aldrich, D.S. Surface characterizations of spin-coated films of Ethylcellulose and MPMC, Langmuir, 23(8), 2007. 001:10.1021/1a0629680</li>
+                <li>Aldrich, D.S. and PPI-EC committee.  For USP Stimuli and Informational Chapter, PARTICULATE MATTER DETERMINATION IN PARENTERAL AND OPHTHALMIC PRODUCTS &lt;1788&gt;, United States Pharmacopeial Convention, Inc., Rockville, Md. 2009.</li>
+                <li>Aldrich D.S.  Chapter 5 - Particulate Matter: Subvisible, in Pharmaceutical Dosage Forms: Parenteral Medications, Nema S and Ludwig JD, eds. Third ed. Volume 2, Informa Healthcare, New York, 114-145, (2010).</li>
+                <li>Aldrich, D.S., Cherris, R.T. and Shabushnig, J.G.  Visual Inspection and Particulate Control, DHI Publishing, LLC, River Grove, IL. (2016).</li>
+                <li>Aldrich, D.S., Cao, S., Hawe, A., Hunt, D.G., Narhi, L., Ripple, D. and Singh, S. “Analytical Gaps and Challenges for Particles in the Submicrometer Size Domain”, Pharm. Forum., 42(6), Nov-Dec 2016.</li>
+                <li>Cherris R, Valley U, Aabye-Hansen L, McLean R, Overroedder D, Owen A, Shabushnig J, Aldrich S, Veillon R, Watson R. PDA Technical Report No. 79, Particulate Matter Control in Difficult to Inspect Parenterals, 2018 Parenteral Drug Association; ISBN: 978-1-945584-02-2.</li>
+                <li>Aldrich D.S.  Chapter 46 - Particulate Matter: Subvisible, in Parenteral Medications, Nema S and Ludwig JD, eds. Fourth ed. CRC Press, Taylor & Francis Group, Boca Raton, FL, 997-1020, (2019).</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Publications;
