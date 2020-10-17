78cbc0d4636050003b4c5b505463c3fbf43e0443
meloegel <meloegel@gmail.com>
2020-10-16 22:57:06 -0400

    fixed contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,22 +7,23 @@ const Contact = () => {
     return (
         <div id='contactPage'>
             <div className='contactDiv'>
-                <h1>D Scott Aldrich<br></br>
+                <div className='contactDivStyle'>
+                    <h1>D Scott Aldrich<br></br>
                 President, Ultramikro, LLC<br></br>
                 Particulate Matter Control Consulting<br></br></h1>
-                <h2>Contact Information</h2>
-                <div className='addrDiv'>
-                    <h3>Address:</h3>
-                    <address>11333 E.D Avenue <br></br>
+                    <h2>Contact Information</h2>
+                    <div className='addrDiv'>
+                        <h3>Address:</h3>
+                        <address>11333 E.D Avenue <br></br>
                         Richland, MI 49083</address>
-                </div>
-                <p className='contactPhone'>Business phone/fax: (269)629-4995</p>
-                <div className='addrDiv'>
-                    <h3>Email:</h3>
-                    <p className='email'>info@ultramikro.com</p>
-                </div>
-                <div id='scottImgDivContact'>
-                    <img id='scottImgContact' src={require('../img/Scott_Photo_2.jpg')} alt='Scott' />
+                    </div>
+                    <p className='contactPhone'>Business phone/fax: (269)629-4995</p>
+                    <div className='addrDiv'>
+                        <p className='email'>Email: info@ultramikro.com</p>
+                    </div>
+                    <div id='scottImgDivContact'>
+                        <img id='scottImgContact' src={require('../img/Scott_Photo_2.jpg')} alt='Scott' />
+                    </div>
                 </div>
             </div>
         </div>
